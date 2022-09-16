95e63e5724cf71a33457a6dcbda187727b25fac3
Cal Courtney <cal@absmartly.com>
2022-09-16 08:55:02 +0100

    Added mergeConfig export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { SDKProvider, useABSmartly } from "./components/SDKProvider";
 import { Treatment } from "./components/Treatment";
+import { mergeConfig } from "@absmartly/javascript-sdk";
 
 export * from "@absmartly/javascript-sdk";
 
-export { Treatment, useABSmartly };
+export { mergeConfig, Treatment, useABSmartly };
 
 export default SDKProvider;
