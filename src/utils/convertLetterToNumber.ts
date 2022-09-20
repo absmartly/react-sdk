98cafb09f18fefa4b9048129bb75ae72537c3be5
Cal Courtney <cal@absmartly.com>
2022-09-20 12:42:14 +0100

    Created letter to number conversion function

---
+++ src/utils/convertLetterToNumber.ts
@@ -0,0 +1,8 @@
+import { Char } from "../types";
+
+export const convertLetterToNumber = (char: Char | number) => {
+  if (typeof char === "number") return char;
+  if (Number.isNaN(parseInt(char)))
+    return char.toLowerCase().charCodeAt(0) - 97;
+  return parseInt(char);
+};
