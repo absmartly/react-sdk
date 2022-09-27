04a8e2d066ef5e5df1d8f2c5958c3b83642f01f6
Cal Courtney <cal@absmartly.com>
2022-09-27 15:04:43 +0100

    Changed jest config to ts

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
+
+export default config;
